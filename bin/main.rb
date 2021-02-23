a42370d781ff63dcdd72e5e0a74a28af241647e0
Billo Diallo <billodiallone@gmail.com>
2021-02-23 15:26:02 +0300

    method to display output,display_prompt

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+puts 'hello'
 require 'colorize'
 # require_relative '../lib/scraper.rb'
 def display_output(array)
@@ -19,3 +21,43 @@ def display_output(array)
     puts '6 ' + (array[17]).to_s.red
     puts '--------------------------'
 end
+
+def display_prompt(summary_info, scraper)
+    puts ''
+    puts 'repositories: ' + (summary_info[7]).to_s.red
+    puts 'stars: ' + (summary_info[9]).to_s.red
+    puts 'followers:' + (summary_info[10]).to_s.red
+    puts 'following: ' + (summary_info[11]).to_s.red
+    puts ''
+    puts 'Enter a category name like ' + 'repository'.red + 'or' + 'stars'.red + 'to get a list of its contents.'
+    puts 'Or Enter ' + '\'q\''.red + ' to exit the program.'
+    puts ''
+    list = scraper.page(gets.chomp)
+    puts '------------------------'
+    list.each.with_index { |item, idx| puts "#{idx + 1}. #{item.red}" }
+    puts '---------------'
+    
+end
+
+puts 'welcome to ! this is github scraping tool'
+    puts ''
+    puts 'All you have to do is input any github username and it shall do the rest.'
+    display_output(Array.new(18,'xxxxx'))
+
+    puts
+
+    puts 'Enter any Github username: '
+
+    begin
+        scraper = Scraper.new(gets.chomp)
+        puts '--------------------------'
+        # puts 'Invalid!, Enter a valid Github ' scraper.valid
+    end until scraper.valid
+    summary_info = scraper.profile_info
+display_output(summary_info)
+
+loop do
+  display_prompt(summary_info, scraper)
+end
+    
+    
